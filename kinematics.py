ddb6ff293da948feb3372c397f3fe54d804613d5
syer <erik.syniawa@informatik.tu-chemnitz.de>
2023-08-16 14:57:37 +0200

    Added a function that checks iCub joint contraints.

--- kinematics.py
+++ kinematics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from parameters import params
 
 # Calculate the Transformation Matrix A according to the DH convention
 def DH_matrix(a, d, alpha, theta, rad=True):
@@ -16,11 +17,17 @@ def DH_matrix(a, d, alpha, theta, rad=True):
     return A
 
 
-def theta_limits(wtheta, atheta):
-    # waist check
+def check_theta_limits(wtheta, atheta, rad=True):
+    if not rad:
+        wtheta = np.radians(wtheta)
+        atheta = np.radians(atheta)
 
+    # waist check
+    waist_ok = all(wtheta >= params['lower_waist_joint_limits']) & all(wtheta <= params['upper_waist_joint_limits'])
     # arm check
-    pass
+    arm_ok = all(atheta >= params['lower_arm_joint_limits']) & all(atheta <= params['upper_arm_joint_limits'])
+
+    return waist_ok & arm_ok
 
 # iCub
 def forward_kinematics(wtheta,
@@ -33,43 +40,48 @@ def forward_kinematics(wtheta,
         wtheta = np.radians(wtheta)
         atheta = np.radians(atheta)
 
-    if arm == 'right':
-        const = -1
-    elif arm == 'left':
-        const = 1
+    if check_theta_limits(wtheta, atheta):
+        if arm == 'right':
+            const = -1
+        elif arm == 'left':
+            const = 1
+        else:
+            raise ValueError('Arm should be either the right arm or the left arm')
+
+        # ---------------------- WAIST ----------------------------
+        # wtheta = [torso_pitch, torso_roll, torso_yaw]
+        A_0 = DH_matrix(a=32, d=0, alpha=np.pi/2, theta=wtheta[0])
+        A_1 = DH_matrix(a=0, d=-5.5, alpha=-np.pi/2, theta=wtheta[1] - np.pi/2)
+        A_2 = DH_matrix(a=const * 23.3647, d=-143.3, alpha=const * -np.pi/2, theta=wtheta[2] + const * 105 * np.pi/180)
+
+        A_01 = A_0 @ A_1
+        A_12 = A_01 @ A_2
+
+        # ---------------------- ARM ----------------------------
+        # atheta = [shoulder_pitch, shoulder_roll, shoulder_yaw, elbow, forearm]
+        # shoulder
+        A_3 = DH_matrix(a=0, d=const * 107.74, alpha=const * -np.pi/2, theta=atheta[0] + const * np.pi/2)
+        A_4 = DH_matrix(a=0, d=0, alpha=np.pi/2, theta=atheta[1] - np.pi/2)
+        A_5 = DH_matrix(a=0, d=const*152.28, alpha=-np.pi/2, theta=atheta[2] - 15*np.pi/180 + const*np.pi/2)
+        # elbow
+        A_6 = DH_matrix(a=-15, d=0, alpha=np.pi/2, theta=atheta[3])
+        #forearm
+        A_7 = DH_matrix(a=0, d=137.3, alpha=np.pi/2, theta=atheta[4] - np.pi/2)
+
+        A_23 = A_12 @ A_3
+        A_34 = A_23 @ A_4
+        A_45 = A_34 @ A_5
+        A_56 = A_45 @ A_6
+        A_67 = A_56 @ A_7
+
+        if return_joint_coordinates:
+            return A_12[:3, 3], A_23[:3, 3], A_56[:3, 3], A_67[:3, 3]
+        else:
+            return A_67[:3, 3]
     else:
-        raise ValueError('Arm should be either the right arm or the left arm')
-
-    # ---------------------- WAIST ----------------------------
-    # wtheta = [torso_pitch, torso_roll, torso_yaw]
-    A_0 = DH_matrix(a=32, d=0, alpha=np.pi/2, theta=wtheta[0])
-    A_1 = DH_matrix(a=0, d=-5.5, alpha=-np.pi/2, theta=wtheta[1] - np.pi/2)
-    A_2 = DH_matrix(a=const * 23.3647, d=-143.3, alpha=const * -np.pi/2, theta=wtheta[2] + const * 105 * np.pi/180)
-
-    A_01 = A_0 @ A_1
-    A_12 = A_01 @ A_2
-
-    # ---------------------- ARM ----------------------------
-    # atheta = [shoulder_pitch, shoulder_roll, shoulder_yaw, elbow, forearm]
-    # shoulder
-    A_3 = DH_matrix(a=0, d=const * 107.74, alpha=const * -np.pi/2, theta=atheta[0] + const * np.pi/2)
-    A_4 = DH_matrix(a=0, d=0, alpha=np.pi/2, theta=atheta[1] - np.pi/2)
-    A_5 = DH_matrix(a=0, d=const*152.28, alpha=-np.pi/2, theta=atheta[2] - 15*np.pi/180 + const*np.pi/2)
-    # elbow
-    A_6 = DH_matrix(a=-15, d=0, alpha=np.pi/2, theta=atheta[3])
-    #forearm
-    A_7 = DH_matrix(a=0, d=137.3, alpha=np.pi/2, theta=atheta[4] - np.pi/2)
-
-    A_23 = A_12 @ A_3
-    A_34 = A_23 @ A_4
-    A_45 = A_34 @ A_5
-    A_56 = A_45 @ A_6
-    A_67 = A_56 @ A_7
-
-    if return_joint_coordinates:
-        return A_12[:3, 3], A_23[:3, 3], A_56[:3, 3], A_67[:3, 3]
-    else:
-        return A_67[:3, 3]
+        return None
 
 if __name__ == '__main__':
     print(forward_kinematics(wtheta = [0,0,0], atheta = np.radians([5, 120, 90, 90, 20])))
+
+    print(theta_limits(wtheta=[0, 0, 0], atheta=np.radians([5, 120, 90, 90, 20])))
